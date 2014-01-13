286f5a8629fe700f9b4b9e372a4502acb09a7658
azhung <azhung.it@gmail.com>
2014-01-12 19:17:52 -0800

    Add Prompt for username input

--- Lession_5/javascript_3.js
+++ Lession_5/javascript_3.js
@@ -1,6 +1,6 @@
 var i = 0;
 var username;
 do {
-	var username = "Please enter you name. Type exit to exit";
+	var username = prompt("Please enter you name. Type exit to exit");
 	document.write(username+". You have run this "+ ++i +" time.");
 } while(username !== "exit");
