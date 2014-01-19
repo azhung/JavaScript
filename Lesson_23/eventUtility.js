0da992e4688ba47b0dffecbbea8056aeeffe7aae
azhung <azhung.it@gmail.com>
2014-01-19 08:58:21 -0800

    Lesson 23 will be learn Writing Cross-Browser Event Code

---
+++ Lesson_23/eventUtility.js
@@ -0,0 +1,43 @@
+var eventUtility = {
+	
+	// Khai báo hàm và cho hàm chạy luôn (function(){}());
+	addEvent : (function(){
+		// Nếu trình duyệt hỗ trợ addEventListener
+		if (typeof addEventListener !== "undefined") {
+			return function (obj, evt, fn) {
+				obj.addEventListener(evt, fn, false);
+			};
+		} 
+		// Nếu trình duyệt không hỗ trợ addEventListener thì sử dụng attachEvent
+		else {
+			return function (obj, evt, fn) {
+				obj.attachEvent("on" + evt, fn);
+			};
+		}
+	}()),
+	
+	removeEvent : (function() {
+		if (typeof addEventListener !== "undefined") {
+			return function (obj, evt, fn) {
+				obj.removeEventListener(evt, fn, false);
+			};
+		} else {
+			return function (obj, evt, fn) {
+				obj.detachEvent("on" + evt, fn);
+			};
+		}
+	}()),
+	
+	getTarget : (function(){
+		if (typeof addEventListener !== "undefined") {
+			return function(event) {
+				return event.target;
+			};
+		} else {
+			return function(event) {
+				return event.srcElement;
+			};
+		}
+	}())
+	
+}
